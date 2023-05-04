3faac5cc050e28f745043a2560607061853bcdc0
Sankalp12DevOps <sravanthi388@gmail.com>
2023-05-04 22:38:03 +0530

    m

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "aws_spot_instance_request" "spot" {
   ami                       = data.aws_ami.my_image.id
   instance_type             = var.INSTANCE_TYPE
   vpc_security_group_ids    = [aws_security_group.allow_tls_prvtappservers.id]
-  subnet_id                 = element(data.terraform_remote_state.vpc.outputs.PRVT_SUBNET_IDS,count.index)
+  subnet_id                 = element(data.terraform_remote_state.vpc.output.PRVT_SUBNET_IDS,count.index)
 
   tags = {
     Name = "${var.COMPONENT}-${var.ENV}-${count.index}-spot"
@@ -16,7 +16,7 @@ resource "aws_instance" "od" {
   ami                           = data.aws_ami.my_image.id
   instance_type                 = var.INSTANCE_TYPE
   vpc_security_group_ids        = [aws_security_group.allow_tls_prvtappservers.id]
-  subnet_id                     = element(data.terraform_remote_state.vpc.outputs.PRVT_SUBNET_IDS,count.index)
+  subnet_id                     = element(data.terraform_remote_state.vpc.output.PRVT_SUBNET_IDS,count.index)
 
   tags = {
      Name = "${var.COMPONENT}-${var.ENV}-${count.index}-od"
