d267feacf4b18e331cf94944fa9598d443e400b7
Sankalp12DevOps <sravanthi388@gmail.com>
2023-05-05 17:03:25 +0530

    m

--- locals.tf
+++ locals.tf
@@ -1,5 +1,5 @@
 locals{
 
 INSTANCE_IDS = concat(aws_spot_instance_request.spot.*.id, aws_instance.od.*.id)
-PRIVATE_IDS = concat(aws_spot_instance_request.spot.*.private_id, aws_instance.od.*.private_id)
+PRIVATE_IDS = concat(aws_spot_instance_request.spot.*.private_ip, aws_instance.od.*.private_ip)
 }
